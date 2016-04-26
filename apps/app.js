1f238f3983690902f91e85fa144645d3fea50653
yolixtly <yolixtly_13@hotmail.com>
2016-04-26 09:41:10 -0400

    less added

--- apps/app.js
+++ apps/app.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
 	console.log('Pirate Bartender');
+
+/*-------------DATABASE. Constructors ------------
+-----(for questions, ingredients, and the pantry)---*/
+//store the ingredients per category: 
+var pantry = {
+	strongIngredients: ["Glug of rum", "slug of whisky", "splash of gin"],
+	saltyingredients: ["Olive on a stick", "salt-dusted rim", "rasher of bacon"],
+	bitterIngredients: ["Shake of bitters", "splash of tonic", "twist of lemon peel"],
+	sweetIngredients: ["Sugar cube", "spoonful of honey", "splash of cola"],
+	fruityIngredients: ["Slice of orange", "dash of cassis", "cherry on top"],
+}
+
+$('.resultDrink').hide();
+
 });
