709ebe31177d408ae8ff0fb162d4ccf0af857cb3
yolixtly <yolixtly_13@hotmail.com>
2016-04-26 10:31:21 -0400

    selected choices

--- apps/app.js
+++ apps/app.js
@@ -4,14 +4,26 @@ $(document).ready(function() {
 /*-------------DATABASE. Constructors ------------
 -----(for questions, ingredients, and the pantry)---*/
 //store the ingredients per category: 
-var pantry = {
+  var pantry = {
 	strongIngredients: ["Glug of rum", "slug of whisky", "splash of gin"],
 	saltyingredients: ["Olive on a stick", "salt-dusted rim", "rasher of bacon"],
 	bitterIngredients: ["Shake of bitters", "splash of tonic", "twist of lemon peel"],
 	sweetIngredients: ["Sugar cube", "spoonful of honey", "splash of cola"],
 	fruityIngredients: ["Slice of orange", "dash of cassis", "cherry on top"],
-}
+  }
 
-$('.resultDrink').hide();
+  $('.resultDrink').hide();
+
+  $('form').on('submit', function(event) {
+		  event.preventDefault();
+
+	  //store the user selected ingredient
+	  var selectedIngredient = [];
+
+	  $('select').each(function() {
+	  	selectedIngredient.push($(this).val() === 'yes' ? true : false);
+  	 });
+	  console.log(selectedIngredient);
+  });
 
 });
